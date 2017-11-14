23ac953df3cd9b2b0024e40dce69f979ebe30be8
Walterooo <covwalter@gmail.com>
2017-11-14 12:44:24 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,14 +4,48 @@
 
 int main() {
 
-    // Parameters
+
+    /* Alle Methoden
+
+    // RekBinFill
+    int ar[3] = { 0, 0, 0};
+
+    rekBinFill(ar, 2, 3, 7);
+
+    // IterBinFill
     int depth = 5;
     int max = pow(2, depth) -1;
     int ar[depth];
 
     fillBinary(ar, depth, max);
 
+    */
+
     return 0;
+}
+
+
+// Mit 0 initialisiertes Array muss übergeben werden
+// idx wird mit depth-1 übergeben. Max is Anzahl von Iterationen
+int rekBinFill( int ar[], int idx, int depth, int max ) {
+
+    if( max == 0 ) {
+        return 0;
+    }
+
+    if( ar[idx] == 0 ) {
+        ar[idx] = 1;
+        printAr( ar, depth );
+        idx = depth-1;
+        max--;
+        rekBinFill( ar, idx, depth, max );
+    }
+
+    if( ar[idx] == 1 ) {
+        ar[idx] = 0;
+        idx--;
+        rekBinFill(ar, idx, depth, max);
+    }
 }
 
 void fillBinary(int ar[], int depth, int max) {
@@ -67,6 +101,14 @@ void printArray( int ar[], int size ) {
     printf("\n");
 }
 
-void print(int x) {
+void printAr( int ar[], int size ) {
+    int i;
+    for( i=0; i<size; ++i ) {
+        print(ar[i]);
+    }
+    printf("\n");
+}
+
+void print( int x ) {
     printf("%d", x);
 }
